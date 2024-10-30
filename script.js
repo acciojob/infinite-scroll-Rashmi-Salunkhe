ee74257abee59da605c86d69bc2a79e730456a4a
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-30 23:18:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+function listItems(val){
+
+	for(let i =0;i<val;i++){
+		const items = document.createElement("li");
+		items.innerText = `Item ${list.children.length +1}`;
+		list.appendChild(items);
+	}
+}
+
+listItems(10);
+
+list.addEventListener("scroll", function(){
+	if(this.clientHeight+ this.scrollTop >= this.scrollHeight){
+		listItems(2);
+	}
+});
